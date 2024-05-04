37efd3e04ecfa257d9100183d7f69028fad6c81d
kbario <kylebario1@gmail.com>
2024-05-04 09:12:29 +0800

    feat(email): init

--- apps/juice-palace/src/routes/event-hire/index.tsx
+++ apps/juice-palace/src/routes/event-hire/index.tsx
@@ -1,51 +1,88 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { Form, routeAction$ } from "@builder.io/qwik-city";
+import EventHireInitialToJP from "@juice-palace/transactional/emails/event-hire-admin";
+import { renderAsync } from "@react-email/render";
 
 export const useAddUser = routeAction$(async (data, requestEvent) => {
-  console.log(data, requestEvent);
-  console.log(requestEvent.env.get("JUICE_PALACE_EVENT_HIRE_EMAIL_API_KEY"));
-  // const sendEmail = async (
-  //     details: { [k: string]: string },
-  //     key: string,
-  //   ): Promise<boolean> => {
-  //     let email;
-  //     try {
-  //       email = makeEmail(details);
-  //     } catch (err) {
-  //       //@ts-ignore
-  //       details["error"] = err;
-  //       email = JSON.stringify(details);
-  //     }
-  //     const res = await fetch("https://api.resend.com/emails", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${key}`,
-  //       },
-  //       body: JSON.stringify({
-  //         from: "kyle@kbar.io",
-  //         to: ["kylebario1@gmail.com", "info@highwycombetavern.com.au"],
-  //         subject: "Gold Card Member Request",
-  //         html: email,
-  //       }),
-  //     });
+  const key = requestEvent.env.get("JUICE_PALACE_EVENT_HIRE_EMAIL_API_KEY");
+  let html: string;
+  let message: string;
+  try {
+    html = await renderAsync(
+      EventHireInitialToJP({
+        enquirerFullName: "John Mathers",
+        enquirerPreferredName: "John",
+        enquirerEmail: "jm@gmail.com",
+        enquirerMessage: "I want coffee at my kids birthday party",
+      }),
+    );
+    message = "good email";
+  } catch (error) {
+    console.log(error);
+    html = JSON.stringify({
+      enquirerFullName: "John Mathers",
+      enquirerPreferredName: "John",
+      enquirerEmail: "jm@gmail.com",
+      enquirerMessage: "I want coffee at my kids birthday party",
+    });
+    message = "bad email";
+  }
+  if (import.meta.env.PROD) {
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      body: JSON.stringify({
+        from: "kyle@kbar.io",
+        to: ["kylebario1@gmail.com"],
+        subject: "Event Hire Request",
+        html,
+      }),
+    });
+    message = res.ok ? "good" : "bad";
+  } else {
+    // const res = await fetch("https://api.resend.com/emails", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${key}`,
+    //   },
+    //   body: JSON.stringify({
+    //     from: "kyle@kbar.io",
+    //     to: ["kylebario1@gmail.com"],
+    //     subject: "Event Hire Request",
+    //     html,
+    //   }),
+    // });
+    // message = res.ok ? "good" : "bad";
+  }
 
-  //     return res.ok;
-  //   };
   return {
     success: true,
+    message,
   };
 });
 
 export default component$(() => {
   const action = useAddUser();
+
+  useVisibleTask$(({ track, cleanup }) => {
+    track(() => action.value?.success);
+    const asdf = setTimeout(() => {
+      if (action.value?.message) action.value.success = false;
+    }, 3000);
+    cleanup(() => clearTimeout(asdf));
+  });
+
   return (
     <div>
       Event Hire
       <Form action={action}>
         <input name="name" />
         <button type="submit">Add user</button>
-        {action.value?.success && <p>User added successfully</p>}
+        {action.value?.success && <p>{action.value.message}</p>}
       </Form>
     </div>
   );
--- apps/juice-palace/vite.config.ts
+++ apps/juice-palace/vite.config.ts
@@ -7,17 +7,15 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
-import { resolve } from "path";
-
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
   dependencies: Record<string, string>;
   devDependencies: Record<string, string>;
   [key: string]: unknown;
 };
-
 /**
  * Note that Vite normally starts from `index.html` but the qwikCity plugin makes start at `src/entry.ssr.tsx` instead.
  */
+
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
@@ -31,15 +29,15 @@ export default defineConfig(({ command, mode }): UserConfig => {
     ssr:
       command === "build" && mode === "production"
         ? {
-          // All dev dependencies should be bundled in the server build
-          noExternal: Object.keys(devDependencies),
-          // Anything marked as a dependency will not be bundled
-          // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-          // If a dep-of-dep needs to be external, add it here
-          // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-          // external: [...Object.keys(dependencies), 'bcrypt']
-          external: Object.keys(dependencies),
-        }
+            // All dev dependencies should be bundled in the server build
+            noExternal: Object.keys(devDependencies),
+            // Anything marked as a dependency will not be bundled
+            // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
+            // If a dep-of-dep needs to be external, add it here
+            // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
+            // external: [...Object.keys(dependencies), 'bcrypt']
+            external: Object.keys(dependencies),
+          }
         : undefined,
     server: {
       headers: {
