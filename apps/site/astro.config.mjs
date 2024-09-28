5595778acf04b2be07cdaf75c213df3ca164712c
kbario <kylebario1@gmail.com>
2024-09-29 07:23:43 +0800

    feat(site): ^ img

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -5,5 +5,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/admin': '/admin/index.html',
+  },
   integrations: [solidJs(), tailwind()],
 });
