1d682ab53b2ee5f77974c1bef3550b2d524a8168
kbario <kylebario1@gmail.com>
2024-10-26 11:06:20 +0800

    feat(site): pwa work

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import { VitePWA } from "vite-plugin-pwa";
+import AstroPWA from "@vite-pwa/astro";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 import react from "@astrojs/react";
@@ -24,8 +24,47 @@ export default defineConfig({
     sitemap(),
     solidJs({ devtools: true, exclude: "src/components/r3f/**" }),
     tailwind(),
-    VitePWA({
+
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["icons/favicon.svg"],
       registerType: "autoUpdate",
+      manifest: {
+        name: "Juice Palace",
+        short_name: "J Palace",
+        theme_color: "#f8fafc",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
     }),
     react(),
     svelte({ include: "src/components/threlte/**" }),
